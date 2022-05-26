4340484652539c5c09bcd028a0e6f2c25d9f35d8
techknowlogick <techknowlogick@gitea.io>
2022-05-26 19:55:42 -0400

    ytt: 0.40.1 -> 0.41.1

--- pkgs/development/tools/ytt/default.nix
+++ pkgs/development/tools/ytt/default.nix
@@ -1,13 +1,13 @@
 { lib, buildGoModule, fetchFromGitHub }:
 buildGoModule rec {
   pname = "ytt";
-  version = "0.40.1";
+  version = "0.41.1";
 
   src = fetchFromGitHub {
     owner = "vmware-tanzu";
     repo = "carvel-ytt";
     rev = "v${version}";
-    sha256 = "sha256-DtzdgEHgxoZRSvylq2vLzU1PAk1ETBDpBWFHcIW95r4=";
+    sha256 = "sha256-JOmDEhisJh4sezxf/Whsf1W7rn4q7C3GqmINQ/A13J0=";
   };
 
   vendorSha256 = null;
